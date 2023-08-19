3d5d22ece89eb6d7f8b3d3366fc7371dc334d949
deepakrana123 <rdev69146@gmail.com>
2023-08-19 09:04:42 +0530

    add

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -11,6 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
+  console.log(user,"user")
 
   if (user && (await user.matchPassword(password))) {
     res.json({
